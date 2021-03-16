c000b46495151e2731c9228103a1490c76da62c8
Derrick Laurente <derricklaurente@192-168-1-107.tpgi.com.au>
2021-03-16 13:37:45 +1100

    finish part 2

--- task2/task2-2.py
+++ task2/task2-2.py
@@ -2,6 +2,8 @@ import re
 
 generic_urls = ["https://www.genericdomain.com/abc/def/1290aodwb23-ghi.img", "https://www.genericdomain.com/abc/31287bdwakj-jkl.img","https://www.genericdomain.com/19unioawd02-jkl.img"]
 
+pattern = r'https://(\w+).(\w+).com/([0-9a-z]{3})?/?([0-9a-z]{3})?/?([0-9a-z]{11})+-(\w+).img'
+
 for url in generic_urls:
-    print(re.match('^[0-9a-z]{11}', url).group(0))
-    special_sequence = [re.match('^[0-9a-z]', url).group(0)]
+    special_sequence = re.sub(pattern, r'\5', url)
+    print(special_sequence)
